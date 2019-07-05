fc0185e75bd0ecdbeb921d8a303217971f939a6d
sapsari <sapsari@gmail.com>
2019-07-05 11:19:21 +0300

    add model with pm

---
+++ BlazorDemo/Pages/Counter.razor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Http;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using MerryYellow.PatternMaker;
+
+namespace BlazorDemo.Pages
+{
+    public class CounterModel : ComponentBase
+    {
+        public string Output = "HELELOY";
+
+        public void Run()
+        {
+            Output += "Y";
+
+            var patterns = Maker.GetPatternList();
+
+            Output += string.Concat(patterns);
+
+            StateHasChanged();
+        }
+
+
+    }
+}
