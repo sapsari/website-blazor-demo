4a2d62bddb5022c0c9afe53ebb6b13a796c903d5
sapsari <sapsari@gmail.com>
2020-11-20 12:24:06 +0300

    update to .net5

--- RoslynWeb/Class1.cs
+++ RoslynWeb/Class1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//#define DISABLE_ROSLYN_BINARIES
+
+using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.IO;
@@ -36,21 +38,33 @@ namespace MerryYellow.RoslynWeb
 
         public static IEnumerable<string> GetPatternList()
         {
+#if DISABLE_ROSLYN_BINARIES
+            return new List<string>() { "PatternTest" };
+#else
             return Maker.GetPatternList();
+#endif
         }
 
         public static IEnumerable<string> GetClassList(string source)
         {
+#if DISABLE_ROSLYN_BINARIES
+            return new List<string>() { "ClassTest" };
+#else
             SystemDlls = null;//**--
             var ws = CreateWorkspace();
+
             var newDoc = ws.AddDocument(ws.CurrentSolution.Projects.First().Id, "myfile.cs", SourceText.From(source));
 
             return Maker.GetClassList(ws);
+#endif
         }
 
         public static string ApplyPattern(string source, string patternName, string className,
             string setting1 = null, string setting2 = null, bool settingA = false, bool settingB = false)
         {
+#if DISABLE_ROSLYN_BINARIES
+            return source;
+#else
             var ws = CreateWorkspace();
             var doc = ws.AddDocument(ws.CurrentSolution.Projects.First().Id, "myfile.cs", SourceText.From(source));
 
@@ -73,6 +87,7 @@ namespace MerryYellow.RoslynWeb
             
             //**--unit test here
             //return PatternMaker.Maker.UnitTestForBlazor(ws.CurrentSolution);
+#endif
         }
 
         public static Stream[] SystemDlls;
@@ -83,15 +98,15 @@ namespace MerryYellow.RoslynWeb
             var projectInfo = ProjectInfo.Create(ProjectId.CreateNewId(), VersionStamp.Create(), "MyProject", "MyProject", LanguageNames.CSharp);
             
             var references = new List<MetadataReference>();
-            if (Maker.IsMonoRuntime)
+            //if (Maker.IsMonoRuntime) // Blazor doesn't run on mono runtime anymore (with .net5)
             {
                 if (SystemDlls != null)
                     references.AddRange(SystemDlls.Select(sd => MetadataReference.CreateFromStream(sd)));
             }
-            else
-            {
-                references.Add(MetadataReference.CreateFromFile(typeof(object).Assembly.Location));
-            }
+            //else
+            //{
+            //    references.Add(MetadataReference.CreateFromFile(typeof(object).Assembly.Location));
+            //}
             projectInfo = projectInfo.WithMetadataReferences(references);
 
             var project = workspace.AddProject(projectInfo);
